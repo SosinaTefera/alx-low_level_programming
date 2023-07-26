3993f2d94c9802b3056aaeb8c09e35a60a1a49b8
SosinaTefera <diyanatefera123@gmail.com>
2023-07-26 13:49:29 -0700

    g

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,8 +1,17 @@
 #include "main.h"
-#include <stdio.h
+#include <stdio.h>
+/**
+ * infinite_add - add 2 strings.
+ * @n1: string1.
+ * @n2: string2.
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: String with all letters in ROT13 base.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
+	
 	while (n1[a_len] != '\0')
 		a_len++;
 	while (n2[b_len] != '\0')
@@ -14,6 +23,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if ((biggest + 1) >= size_r)
 		return (0);
 	r[biggest + 1] = '\0';
+	
 	while (biggest >= 0)
 	{
 		a = (n1[a_len - 1] - '0');
@@ -26,6 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum = a + carry;
 		else
 			sum = carry;
+		
 		if (sum > 9)
 		{
 			carry = sum / 10;
